5118f6184713f711e83780a9fea1ce3e962cd024
Wayne Bell <117953075+TherealWayne@users.noreply.github.com>
2023-02-20 15:31:14 -0500

    Add files via upload

---
+++ project_mod5/my_sum/__init__.py
@@ -0,0 +1,9 @@
+#Homework for module 5 of SDEV220
+# link to homework: https://realpython.com/python-testing/#automated-vs-manual-testing 
+
+
+def sum(arg):
+    total = 0
+    for val in arg:
+        total += val
+    return total 
